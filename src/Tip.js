5d9065e6dfc919a6e484f2a74868064f98b609e7
PrabhashDiss <101409417+PrabhashDiss@users.noreply.github.com>
2023-07-13 12:16:32 +0530

    Update Tip.js

--- src/Tip.js
+++ src/Tip.js
@@ -3,9 +3,6 @@ import React from 'react';
 const Tip = ({ tip }) => {
   return (
     <div class="card">
-      <div class="card-header">
-        Tip!
-      </div>
       <div class="card-body">
         <h5 class="card-title">Special tip treatment</h5>
         <p class="card-text">{tip}</p>
