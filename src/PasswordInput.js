5ac4b6c787518f642974a2b70894983c8212e590
PrabhashDiss <101409417+PrabhashDiss@users.noreply.github.com>
2023-07-13 11:48:53 +0530

    Update PasswordInput.js

--- src/PasswordInput.js
+++ src/PasswordInput.js
@@ -3,14 +3,11 @@ import React from 'react';
 const PasswordInput = ({ value, onChange, onSubmit }) => {
   return (
     <form onSubmit={onSubmit}>
-      <label htmlFor="password">Enter Password:</label>
-      <input
-        type="password"
-        id="password"
-        value={value}
-        onChange={onChange}
-      />
-      <button type="submit">Submit</button>
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon2">Enter Password</span>
+        <input type="text" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon2" value={value} onChange={onChange}/>
+        <button type="button" class="btn btn-outline-success">Submit</button>
+      </div>
     </form>
   );
 };
