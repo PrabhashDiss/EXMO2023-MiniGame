0665227b9f86204276d26d8b8684ae80581a8c6b
PrabhashDiss <101409417+PrabhashDiss@users.noreply.github.com>
2023-07-13 11:55:32 +0530

    Update Tip.js

--- src/Tip.js
+++ src/Tip.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Tip = ({ tip }) => {
-  return <p>{tip}</p>;
+  return (
+    <div class="card">
+      <div class="card-header">
+        Tip!
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Special tip treatment</h5>
+        <p class="card-text">{tip}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Tip;
