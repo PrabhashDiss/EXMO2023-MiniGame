5c0841a268243b2d9334cf7987c8f2fa163bf5fe
PrabhashDiss <101409417+PrabhashDiss@users.noreply.github.com>
2023-07-13 12:16:54 +0530

    Update Tip.js

--- src/Tip.js
+++ src/Tip.js
@@ -4,7 +4,7 @@ const Tip = ({ tip }) => {
   return (
     <div class="card">
       <div class="card-body">
-        <h5 class="card-title">Special tip treatment</h5>
+        <h5 class="card-title">Tip!</h5>
         <p class="card-text">{tip}</p>
       </div>
     </div>
